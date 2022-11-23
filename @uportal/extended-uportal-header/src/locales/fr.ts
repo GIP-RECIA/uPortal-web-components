068994015d1f411dcac8b660f4a122434177f02d
gbrousse-recia <gregory.brousse@recia.fr>
2022-11-23 16:00:59 +0100

    feat(extended-uportal-header): sign in capacity

--- @uportal/extended-uportal-header/src/locales/fr.ts
+++ @uportal/extended-uportal-header/src/locales/fr.ts
@@ -1,10 +1,13 @@
 // Do not modify this file by hand!
 // Re-generate this file by running lit-localize
 
+import { str } from '@lit/localize';
+
 /* eslint-disable no-irregular-whitespace */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const templates = {
-  s02ea2ba04ac778bf: `retour au portail`,
-  s98512c9b90146e3c: `icône du portail`,
+  s02ea2ba04ac778bf: str`Retour au portail`,
+  s03c75db6e18f29d2: str`Se connecter`,
+  s98512c9b90146e3c: str`Icone du portail`,
 };
